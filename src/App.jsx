46c3c17b023cae975a623c443415fa0e7720e427
Andrea <andrea@interhact.net>
2024-04-17 12:48:22 -0500

    initial routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Components/AppElements/HeaderContainer';
+import Aside from './Components/AppElements/AsideContainer';
+import Main from './Components/AppElements/MainContainer';
+import Nav from './Components/AppElements/NavContainer';
+import Footer from './Components/AppElements/NavContainer';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <div style={{ display: 'flex' }}>
+        <Aside />
+        <Main />
+        <Nav />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Footer />
+    </div>
+  </Router>
+);
 
-export default App
+export default App;
