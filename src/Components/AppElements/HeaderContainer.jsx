74d85389bb665cf9584275a8cabc80d2788e1ead
Andrea <andrea@interhact.net>
2024-04-17 13:02:16 -0500

    setup for header container routing

---
+++ src/Components/AppElements/HeaderContainer.jsx
@@ -0,0 +1,19 @@
+import { useLocation } from 'react-router-dom';
+
+import LargeHeader from '../HeaderPane/LargeHeader';
+import SimpleHeader from '../HeaderPane/SimpleHeader';
+
+const routeToHeader = {
+  '/dashboard': LargeHeader,
+  '/profile': LargeHeader,
+  '/reading': SimpleHeader,
+};
+
+const Header = () => {
+  const location = useLocation();
+  const HeaderComponent = routeToHeader[location.pathname];
+
+  return <div>{HeaderComponent && <HeaderComponent />}</div>;
+};
+
+export default Header;
