d97c52a3ccb01f3cd70871be0951a0c54a0ddb2c
Andrea <andrea@interhact.net>
2024-04-18 10:15:05 -0500

    added logout button on navigation links component to simulate returning to the landing page on logout

--- src/Components/NavigationPane/NavigationLinks.jsx
+++ src/Components/NavigationPane/NavigationLinks.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 const NavigationLinks = () => {
   return (
-    <nav style={{ height: '100%', width: '150px', backgroundColor: 'grey' }}>
-      <ul style={{ listStyleType: 'none', padding: 0, textAlign: 'center' }}>
+    <nav style={{ height: '100%', width: '150px', backgroundColor: 'grey', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
+      <ul style={{ listStyleType: 'none', padding: 0, textAlign: 'center', marginBottom: '20px' }}>
         <li>
           <Link to="/dashboard" style={{ textDecoration: 'none', color: 'darkslategrey' }}>Dashboard</Link>
         </li>
@@ -11,6 +11,7 @@ const NavigationLinks = () => {
           <Link to="/profile" style={{ textDecoration: 'none', color: 'darkslategrey' }}>Profile</Link>
         </li>
       </ul>
+      <Link to="/" style={{ textDecoration: 'none', color: 'darkslategrey', backgroundColor: 'lightgrey', padding: '10px', textAlign: 'center', borderRadius: '5px', width: '100px',marginBottom: '.5rem' }}>Logout</Link>
     </nav>
   );
 };
