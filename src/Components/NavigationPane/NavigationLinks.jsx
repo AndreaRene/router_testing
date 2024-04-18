bcacba4ed77ab7807ddc0bf0eea65c3540689b43
Andrea <andrea@interhact.net>
2024-04-18 14:37:12 -0500

    setup nav links for new pages

--- src/Components/NavigationPane/NavigationLinks.jsx
+++ src/Components/NavigationPane/NavigationLinks.jsx
@@ -3,22 +3,38 @@ import { Link } from 'react-router-dom';
 const NavigationLinks = () => {
   return (
     <nav style={ { height: '100%', width: '150px', backgroundColor: 'grey', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' } }>
-      <ul style={ { listStyleType: 'none', padding: 0, textAlign: 'center', marginBottom: '20px' } }>
-        <li style={ { marginBottom: '0.4rem'} }>
-          <Link to="/dashboard" style={ { textDecoration: 'none', color: 'darkslategrey'} }>Dashboard</Link>
-        </li>
-        <li style={ { marginBottom: '0.4rem'} }>
-          <Link to="/newreading" style={ { textDecoration: 'none', color: 'darkslategrey' } }>New Reading</Link>
-        </li>
-        <li style={ { marginBottom: '0.4rem'} }>
-          <Link to="/profile" style={ { textDecoration: 'none', color: 'darkslategrey' } }>Profile</Link>
-        </li>
-        <li>
-          <Link to="/community" style={ { textDecoration: 'none', color: 'darkslategrey' } }>Community</Link>
-        </li>
-      </ul>
+      <section>
+        <ul style={ { listStyleType: 'none', padding: 0, textAlign: 'center' } }>
+          <li style={ { marginBottom: '0.4rem' } }>
+            <Link to="/dashboard" style={ { textDecoration: 'none', color: 'darkslategrey' } }>Dashboard</Link>
+          </li>
+          <li style={ { marginBottom: '0.4rem' } }>
+            <Link to="/newreading" style={ { textDecoration: 'none', color: 'darkslategrey' } }>New Reading</Link>
+          </li>
+          <li style={ { marginBottom: '0.4rem' } }>
+            <Link to="/profile" style={ { textDecoration: 'none', color: 'darkslategrey' } }>Profile</Link>
+          </li>
+          <li>
+            <Link to="/community" style={ { textDecoration: 'none', color: 'darkslategrey' } }>Community</Link>
+          </li>
+        </ul>
+        <hr />
+        <ul style={ { listStyleType: 'none', padding: 0, textAlign: 'center'} }>
+          <li style={ { marginBottom: '0.4rem' } }>
+            <Link to="/dashboard" style={ { textDecoration: 'none', color: 'darkslategrey' } }>Browse Spreads</Link>
+          </li>
+          <li style={ { marginBottom: '0.4rem' } }>
+            <Link to="/newreading" style={ { textDecoration: 'none', color: 'darkslategrey' } }>Browse Decks</Link>
+          </li>
+          <li style={ { marginBottom: '0.4rem' } }>
+            <Link to="/profile" style={ { textDecoration: 'none', color: 'darkslategrey' } }>App Shop</Link>
+          </li>
+        </ul>
+        <hr />
+      </section>
       <Link to="/" style={ { textDecoration: 'none', color: 'darkslategrey', backgroundColor: 'lightgrey', padding: '10px', textAlign: 'center', borderRadius: '5px', width: '100px', marginBottom: '.5rem' } }>Logout</Link>
     </nav>
+
   );
 };
 
