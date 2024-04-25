eced9cc3eb67e70e1347f744e56bd7ab7a341cb2
AndreaRene <andrea@interhact.net>
2024-04-24 20:42:43 -0500

    troubleshooting card flip

--- src/Pages/NewReading/NewReading.jsx
+++ src/Pages/NewReading/NewReading.jsx
@@ -1,11 +1,14 @@
+import Card from '../../Components/CardComponents/TallRectangleCards/TallRectangleTarotCard.jsx'
+
 const NewReading = () => {
 
 
     return (
         <section>
-
+            <Card />
         </section>
     );
 };
 
 export default NewReading;
+
